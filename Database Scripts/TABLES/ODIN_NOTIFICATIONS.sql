709b2432d9acc35fce8dfc9cfc08293afbea99d3
MBLedbetter <bledbetter@trendsinternational.com>
2018-09-07 08:43:29 -0400

    published

--- Database Scripts/TABLES/ODIN_NOTIFICATIONS.sql
+++ Database Scripts/TABLES/ODIN_NOTIFICATIONS.sql
@@ -9,6 +9,7 @@ INSERT INTO ODIN_NOTIFICATIONS VALUES (5, '2018-27-17 00:00:00.000', 'A Product 
 INSERT INTO ODIN_NOTIFICATIONS VALUES (6, '2018-08-08 00:00:00.000', 'Product Id Translations can no longer be modified after an items initial setup. Orders in progress will error if this field is adjusted. Ecommerce Image paths in excel sheets will no longer be read into Odin and will be generated from the Image Path fields.')
 INSERT INTO ODIN_NOTIFICATIONS VALUES (7, '2018-08-21 00:00:00.000', 'Sell On Guitar Center field has been added.')
 INSERT INTO ODIN_NOTIFICATIONS VALUES (8, '2018-08-28 00:00:00.000', 'Bill of Material values can no longer be updated once they have been inserted into the system. Orders in progress will error if this field is adjusted.')
+INSERT INTO ODIN_NOTIFICATIONS VALUES (9, '2018-09-07 00:00:00.000', 'Sell On Amazon Seller Central has been added.')
 */
 SET ANSI_NULLS ON
 GO
