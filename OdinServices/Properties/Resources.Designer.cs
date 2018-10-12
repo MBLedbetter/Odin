9fc746b396aec5156c52f5ec826c0b16ea369b30
MBLedbetter <bledbetter@trendsinternational.com>
2018-10-12 15:52:57 -0400

    ftp password update

--- OdinServices/Properties/Resources.Designer.cs
+++ OdinServices/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace OdinServices.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -178,7 +178,7 @@ namespace OdinServices.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to jTp\Zkqw:rQ@@x7m.
+        ///   Looks up a localized string similar to Jr75#wQw@@pKv.
         /// </summary>
         internal static string ftpPassword {
             get {
