59d37fb3d8fe10fff04a91bb1483ec0d3751a609
MBLedbetter <bledbetter@trendsinternational.com>
2019-01-29 14:33:40 -0500

    removed testing messagebox before i forget about it

--- OdinServices/ItemService.cs
+++ OdinServices/ItemService.cs
@@ -4963,7 +4963,6 @@ namespace OdinServices
                     break;
                 case "CAD":
                     value = var.ListPriceCad;
-                    MessageBox.Show(value);
                     break;
                 case "MXN":
                     value = var.ListPriceMxn;
