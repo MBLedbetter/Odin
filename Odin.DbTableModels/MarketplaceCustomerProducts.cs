d93e740908ccfdf1fb8007b0e7b92ffffa29bd68
MBLedbetter <bledbetter@trendsinternational.com>
2019-06-11 08:25:33 -0400

    added stuff

---
+++ Odin.DbTableModels/MarketplaceCustomerProducts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin.DbTableModels
+{
+    public class MarketplaceCustomerProducts
+    {
+        #region Properties
+        
+        /// <summary>
+        ///     Gets or sets CUST_ID
+        /// </summary>
+        public string CustId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets INV_ITEM_ID
+        /// </summary>
+        public string InvItemId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets SETID
+        /// </summary>
+        public string Setid { get; set; }
+
+        /// <summary>
+        ///     Gets or sets TITLE
+        /// </summary>
+        public string Title { get; set; }
+
+        #endregion // Properties
+    }
+}
